cbf7d42cefaa7d04f4edae488805385fbeddff9a
sanjaySDET <sanjaybabusdet@gmail.com>
2024-10-23 13:13:16 +0530

    1

---
+++ Demo/src/test/java/Demo/TestScript1Test.java
@@ -0,0 +1,21 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class TestScript1Test {
+
+	@Test
+	public void m1() {
+
+		//welcome
+		System.out.println("Hi");
+		
+		System.out.println("Vanakaaam sir ");
+
+		//Thank you
+		//completed
+
+		//Test2
+	}
+
+}
---
+++ Demo/src/test/java/Demo/TestScript2.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class TestScript2 {
+
+	@Test
+	public void m1()
+	{
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("hello");
+		
+		//Thank you
+	}
+}
